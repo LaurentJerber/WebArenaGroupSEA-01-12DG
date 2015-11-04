0eb06f718ffdf8432674da64bf73cbf26cca0c2f
Laurent Jerber <laurent.jerber@gmail.com>
2015-11-04 15:30:49 +0100

    Arenas

--- app/Controller/ArenasController.php
+++ app/Controller/ArenasController.php
@@ -53,6 +53,10 @@ class ArenasController extends AppController
 	
 	public function diary() {
 		$this -> set('raw',$this->Event->find());
+	
+		//2014-11-07 12:00:00
+		$ilya24h = date("Y-m-d H:i:s", time() - (60*60*24));
+		$events = $this -> query("SELECT * FROM events WHERE date > " . $ilya24h);
 	}
 }
 ?>
